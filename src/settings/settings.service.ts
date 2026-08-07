38eb2b43717604d62531674e4f6c430d142fc6ca
Tiago Santos <tiagodevss@gmail.com>
2026-08-07 16:17:36 -0300

    .

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -4,13 +4,31 @@ import { UpdateSettingsDto } from './update-settings.dto';
 
 export const INVITE_MESSAGE_TEMPLATE_KEY = 'invite_message_template';
 
-export const DEFAULT_INVITE_MESSAGE_TEMPLATE = `Olá, {{nome}}! 💛
-Vocês estão convidados para o nosso casamento.
-Confirmem a presença por aqui: {{link}}`;
+export const DEFAULT_INVITE_MESSAGE_TEMPLATE = `Queridos {{nome}},🤍
 
-export const DEFAULT_INVITE_MESSAGE_TEMPLATE_SINGLE = `Olá, {{nome}}! 💛
-Você está convidado para o nosso casamento.
-Confirme a presença por aqui: {{link}}`;
+Nosso grande dia está cada vez mais próximo, e estamos muito felizes por poder compartilhar esse momento tão especial com vocês.
+
+Nosso site do casamento já está disponível! Nele, vocês encontrarão todas as informações sobre a cerimônia e a recepção, além de poderem confirmar sua presença.
+
+Pedimos, com carinho, que realizem a confirmação para nos ajudar na organização desse dia tão sonhado até 01/10/2026✨
+
+✨ Acesse nosso site:
+{{link}}
+
+Será uma alegria imensa celebrar esse momento ao lado de pessoas tão especiais. Esperamos por vocês! 🤍`;
+
+export const DEFAULT_INVITE_MESSAGE_TEMPLATE_SINGLE = `Querido(a) {{nome}},🤍
+
+Nosso grande dia está cada vez mais próximo, e estamos muito felizes por poder compartilhar esse momento tão especial com você.
+
+Nosso site do casamento já está disponível! Nele, você encontrará todas as informações sobre a cerimônia e a recepção, além de poder confirmar sua presença.
+
+Pedimos, com carinho, que realize a confirmação para nos ajudar na organização desse dia tão sonhado até 01/10/2026✨
+
+✨ Acesse nosso site:
+{{link}}
+
+Será uma alegria imensa celebrar esse momento ao lado de pessoas tão especiais. Esperamos por você! 🤍`;
 
 @Injectable()
 export class SettingsService {
